49041a0a8fd56567be6d742ff90347224c2bef18
niuee <vntchang@gmail.com>
2023-05-28 18:32:23 +0800

    Built a new distributed index.js

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,65 @@
-export declare function testFunc(): void;
+export type point = {
+    x: number;
+    y: number;
+    z?: number;
+};
+export declare class PointCal {
+    static addVector(a: point, b: point): {
+        x: number;
+        y: number;
+        z?: undefined;
+    } | {
+        x: number;
+        y: number;
+        z: number;
+    };
+    static subVector(a: point, b: point): {
+        x: number;
+        y: number;
+        z?: undefined;
+    } | {
+        x: number;
+        y: number;
+        z: number;
+    };
+    static multiplyVectorByScalar(a: point, b: number): {
+        x: number;
+        y: number;
+        z?: undefined;
+    } | {
+        x: number;
+        y: number;
+        z: number;
+    };
+    static divideVectorByScalar(a: point, b: number): {
+        x: number;
+        y: number;
+        z?: undefined;
+    } | {
+        x: number;
+        y: number;
+        z: number;
+    };
+    static magnitude(a: point): number;
+    static unitVector(a: point): {
+        x: number;
+        y: number;
+        z: number;
+    };
+    static dotProduct(a: point, b: point): number;
+    static crossProduct(a: point, b: point): {
+        x: number;
+        y: number;
+        z: number;
+    };
+    static unitVectorFromA2B(a: point, b: point): {
+        x: number;
+        y: number;
+        z: number;
+    };
+    static rotatePoint(point: point, angle: number): {
+        x: number;
+        y: number;
+    };
+    static angleFromA2B(a: point, b: point): number;
+}
