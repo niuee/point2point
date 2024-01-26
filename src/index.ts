637701f5e44b1aeafaca88579386ffa5f903af0b
Vincent Chang <vntchang@gmail.com>
2024-01-26 12:37:41 +0800

    Add a function to get line intersections

--- src/index.ts
+++ src/index.ts
@@ -117,6 +117,32 @@ export class PointCal {
         }
         return a.x == b.x && a.y == b.y && a.z == b.z;
     }
+
+    static getLineIntersection(startPoint: Point, endPoint: Point, startPoint2: Point, endPoint2: Point):{
+        intersects: boolean,
+        intersection?: Point,
+        offset?: number
+    }{
+        const numerator = (endPoint2.x - startPoint2.x) * (startPoint.y - startPoint2.y) - (endPoint2.y - startPoint2.y) * (startPoint.x - startPoint2.x);
+        const denominator = (endPoint2.y - startPoint2.y) * (endPoint.x - startPoint.x) - (endPoint2.x - startPoint2.x) * (endPoint.y - startPoint.y);
+        
+        if (denominator === 0){
+            return {intersects: false};
+        }
+        const t = numerator / denominator;
+        if (t >= 0 && t <= 1){
+            return {
+                intersects: true, 
+                intersection: PointCal.linearInterpolation(startPoint, endPoint, t),
+                offset: t
+            }
+        } else {
+            return {
+                intersects: false,
+            }
+        }
+    
+    }
     
 }
     
