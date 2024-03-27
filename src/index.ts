1f3292c37eef97cbaad059ef0417271f6a2f3690
Vincent Chang <vntchang@gmail.com>
2024-03-27 17:30:16 +0800

    Explicitly add return types for functions

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ export type Point = {
 
 export class PointCal {
 
-    static addVector(a: point, b: point) {
+    static addVector(a: point, b: point): Point {
         if (a.z == null && b.z == null) return {x: a.x + b.x, y: a.y + b.y};
         if (a.z == null || b.z == null) {
             if (a.z == null) a.z = 0;
@@ -22,7 +22,7 @@ export class PointCal {
         return {x: a.x + b.x, y: a.y + b.y, z: a.z + b.z}; 
     }
 
-    static subVector(a: point, b: point) {
+    static subVector(a: point, b: point): Point {
         if (a.z == null && b.z == null) return {x: a.x - b.x, y: a.y - b.y};
         if (a.z == null || b.z == null) {
             if (a.z == null) a.z = 0;
@@ -31,28 +31,28 @@ export class PointCal {
         return {x: a.x - b.x, y: a.y - b.y, z: a.z - b.z};
     }
 
-    static multiplyVectorByScalar(a: point, b: number) {
+    static multiplyVectorByScalar(a: point, b: number): Point {
         if (a.z == null) return {x: a.x * b, y: a.y * b};
         return {x: a.x * b, y: a.y * b, z: a.z * b};
     }
 
-    static divideVectorByScalar(a: point, b: number) {
+    static divideVectorByScalar(a: point, b: number): Point {
         if (b == 0) return {x: a.x, y: a.y};
         if (a.z == null) return {x: a.x / b, y: a.y / b};
         return {x: a.x / b, y: a.y / b, z: a.z / b};
     }
 
-    static magnitude(a: point) {
+    static magnitude(a: point): number {
         if (a.z == null) a.z = 0;
         return Math.sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
     }
 
-    static unitVector(a: point) {
+    static unitVector(a: point): Point {
         if (a.z == null) a.z = 0;
         return this.magnitude(a) != 0 ? {x: a.x / this.magnitude(a), y: a.y / this.magnitude(a), z: a.z / this.magnitude(a)} : {x: 0, y: 0, z: 0};
     }
 
-    static dotProduct(a: point, b: point) {
+    static dotProduct(a: point, b: point): number {
         if (a.z == null && b.z == null) return a.x * b.x + a.y * b.y;
         if (a.z == null || b.z == null) {
             if (a.z == null) a.z = 0;
@@ -61,7 +61,7 @@ export class PointCal {
         return a.x * b.x + a.y * b.y + a.z * b.z;
     }
 
-    static crossProduct(a: point, b: point) {
+    static crossProduct(a: point, b: point): Point {
         if (a.z == null || b.z == null) {
             if (a.z == null) a.z = 0;
             if (b.z == null) b.z = 0;
@@ -69,34 +69,34 @@ export class PointCal {
         return {x: a.y * b.z - a.z * b.y, y: a.z * b.x - a.x * b.z, z: a.x * b.y - a.y * b.x};
     }
 
-    static unitVectorFromA2B(a: point, b: point) {
+    static unitVectorFromA2B(a: point, b: point): Point {
         return this.unitVector(this.subVector(b, a));
     }
 
-    static rotatePoint(point: point, angle: number) {
+    static rotatePoint(point: point, angle: number): Point {
         return {x: point.x * Math.cos(angle) - point.y * Math.sin(angle), y: point.x * Math.sin(angle) + point.y * Math.cos(angle)};
     }
 
-    static transform2NewAxis(point: point, angleFromOriginalAxis2DestAxis: number) {
+    static transform2NewAxis(point: point, angleFromOriginalAxis2DestAxis: number): Point {
         // angle is the angle from the original axis to the destination axis ccw is positive as always
         return {x: point.x * Math.cos(angleFromOriginalAxis2DestAxis) + point.y * Math.sin(angleFromOriginalAxis2DestAxis), y: -point.x * Math.sin(angleFromOriginalAxis2DestAxis) + point.y * Math.cos(angleFromOriginalAxis2DestAxis)};
     }
 
-    static angleFromA2B(a: point, b: point) {
+    static angleFromA2B(a: point, b: point): number {
         return Math.atan2(a.x * b.y - a.y * b.x, a.x * b.x + a.y * b.y);
     }
 
-    static transformPointWRTAnchor(point: point, anchor: point, angle: number) {
+    static transformPointWRTAnchor(point: point, anchor: point, angle: number): Point {
         // angle is in radians
         let newPoint = this.rotatePoint(this.subVector(point, anchor), angle);
         return this.addVector(newPoint, anchor);
     }
 
-    static distanceBetweenPoints(a: point, b: point) {
+    static distanceBetweenPoints(a: point, b: point): number {
         return this.magnitude(this.subVector(a, b));
     }
 
-    static flipYAxis(point: point): point{
+    static flipYAxis(point: point): Point{
         return {x: point.x, y: -point.y, z: point.z};
     }
 
